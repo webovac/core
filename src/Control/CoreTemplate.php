19cade539d426119b979f3e35f35f7d9718b9955
Štěpán Postler <stepan.postler@gmail.com>
2024-06-02 17:38:40 +0200

    wip

--- src/Control/CoreTemplate.php
+++ src/Control/CoreTemplate.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Webovac\Core\Control;
 
+use Latte\Attributes\TemplateFilter;
 use Nette\Application\UI\Control;
 use Nette\Application\UI\Presenter;
 use Nette\Security\User;
@@ -17,4 +18,15 @@ trait CoreTemplate
 	public string $baseUrl;
 	public string $basePath;
 	public array $flashes = [];
+
+
+	#[TemplateFilter]
+	public function replacePlaceholder(string $s): string
+	{
+		return str_replace(
+			'{LOGGED_USER_NAME}',
+			$this->loggedPerson?->name ?: '',
+			$s
+		);
+	}
 }
