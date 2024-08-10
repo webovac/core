fd1285724574b1bca618f72bbedadf6ad6259f9b
Štěpán Postler <stepan.postler@fsv.cvut.cz>
2024-08-10 17:19:29 +0200

    wip

--- src/Model/QueryName/CoreQueryNameRepository.php
+++ src/Model/QueryName/CoreQueryNameRepository.php
@@ -7,12 +7,13 @@ namespace Webovac\Core\Model\QueryName;
 use App\Model\Page\Page;
 use App\Model\PageTranslation\PageTranslation;
 use App\Model\PageTranslation\PageTranslationData;
+use App\Model\QueryName\QueryName;
 use App\Model\QueryName\QueryNameData;
 
 
 trait CoreQueryNameRepository
 {
-	public function getByData(QueryNameData|string $data, Page $page): ?PageTranslation
+	public function getByData(QueryNameData|string $data, Page $page): ?QueryName
 	{
 		return $this->getBy(['page' => $page, 'query' => $data instanceof QueryNameData ? $data->query : $data]);
 	}
