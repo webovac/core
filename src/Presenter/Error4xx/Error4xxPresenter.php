d68523575b8bc1933197ab8866d30ef96db228b7
Štepán Postler <stepan.postler@fsv.cvut.cz>
2024-11-14 12:24:29 +0100

    wip

--- src/Presenter/Error4xx/Error4xxPresenter.php
+++ src/Presenter/Error4xx/Error4xxPresenter.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace Webovac\Core\Presenter\Error4xx;
 
 use Nette\Application\UI\Presenter;
-use Webovac\Auth\Presenter\AuthPresenter;
 use Webovac\Core\Presenter\CorePresenter;
 use Webovac\Core\Presenter\ErrorPresenter;
-use Webovac\Search\Presenter\SearchPresenter;
 use Webovac\Style\Presenter\StylePresenter;
 
 
@@ -20,6 +18,4 @@ class Error4xxPresenter extends Presenter
 	use ErrorPresenter;
 	use CorePresenter;
 	use StylePresenter;
-	use SearchPresenter;
-	use AuthPresenter;
 }
